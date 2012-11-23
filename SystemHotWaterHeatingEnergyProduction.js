95b7102b83dcdfd4badd1fb411b678042006e4d6
Mikko Ahlström <mikko.ahlstrom@gmail.com>
2012-11-23 14:40:41 +0200

    Missing system profile generator functions created

---
+++ SystemHotWaterHeatingEnergyProduction.js
@@ -0,0 +1,8 @@
+function SystemHotWaterHeatingEnergyProduction (system,constants) {
+	var hour;
+	systemProfile = new Profile();
+	for(hour=0;hour<8760;hour++) {
+		systemProfile.profile[hour] = 0.0;
+	}
+	return systemProfile;
+}
